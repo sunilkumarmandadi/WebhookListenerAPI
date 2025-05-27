105e2efd087a8d02f683006f3a75e7b4e7cfa223
Sunilkumar Mandati <sunil.mandati@gls-us.com>
2025-05-27 07:20:13 -0700

    Add project files.

---
+++ Controllers/TransiteventListenerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebhookListenerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransiteventListenerController : ControllerBase
+    {
+        public static WebhookRequest _dataStore = new WebhookRequest();
+        [HttpPost]
+        public IActionResult Receive([FromBody] WebhookRequest request)
+        {
+            // Save the shipment or trigger processing logic
+            
+            _dataStore.Events = request.Events;
+            return Ok(new { message = "Shipment received" });
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (_dataStore.Events == null)
+            {
+                return NotFound(new { message = "No shipment data found" });
+            }
+            return Ok(_dataStore.Events);
+        }
+    }
+}
---
+++ TransitEvents.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WebhookListenerAPI
+{
+    public class TransitEvents
+    {
+        public string TrackingNumber { get; set; }
+        public string EventDateTime { get; set; }
+        public string EventDetails { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+    public class WebhookRequest
+    {
+        public List<TransitEvents> Events { get; set; }
+    }
+}
